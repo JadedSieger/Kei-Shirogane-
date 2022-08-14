1d95e98a45db9e77a40ccf512bb1abe43d80817b
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2022-08-14 22:15:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const { LoadCommands, Bot }= require("aoi.js");
-const config = require("./config.json");
+//OTg0NzY1NDAyMjU5MjY3NjI0.GS7jR0.q9hCYPgFR5svjAyycubRbbDZXjp8A5wuDRA7PE
+//Bot files
+const { LoadCommands, Bot } = require("aoi.js");
+const config = require('./config.json');
 const keepAlive = require("./server.js");
 
+
 const bot = new Bot({
-sharding: false,
-token: config.token,
-prefix: [config.token1, config.token2, config.token3],
-intents: ["guilds", "guildMessage"]
+  token: config.token,
+  prefix: [config.prefix_1,config.prefix_2,config.prefix_3],
+  intents: ["guilds", "guildMessages", "guildVoiceStates"],
 });
 
 const loader = new LoadCommands(bot);
 
-//Ready Event:
+//Ready Event
 bot.readyCommand({
-    channel: " ",
-    code:`
-    _Initializing systems..._
+  channel: "989758108568023072",
+  code: `
+_Initializing systems..._
 $editIn[3s;██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ 10%;███▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ 15%;█████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ 25%;████████▁▁▁▁▁▁▁▁▁▁▁▁ 40%;███████████▁▁▁▁▁▁▁▁▁ 55%;██████████████▁▁▁▁▁▁ 70%;██████████▄▁ 87%;█████████████▇ 99%;██████████ 100%;
 
 
@@ -24,31 +26,84 @@ _loaded "./Commands/fun/"_
 _loaded "./Commands/moderation/"_
 
 _checking server-side ping: $pingms_
-**_Kei Shirogane v2.1.0a ready._**]
-    `
-});
+**_Kei Shirogane v2.1.0a ready._**]`
+})
 
-//this is a normal bot command that does not use command handlers.
+//testing commands
 bot.command({
-    name:"ping",
-    code:`
-    Griffin and Kryuger Database sent a response in $pingms
-    `
-});
+  name: "ping",
+  code: `ponging at $pingms`
+})
+bot.command({
+name: "slashcreate",
+code: `
+$createApplicationCommand[$guildID;test;a cool slash command for aoijs;true;slash;{
+               "name" : "test",
+               "description" : "sends a message",
+               "type" : 3,
+               "required" : true
+      }]`
+//This will make our slash command
+})
 
-//Sets status of the bot
+bot.command({
+name: "slash-say",
+code: `
+$createApplicationCommand[$guildID;say;a cool slash command for aoijs;true;slash;{
+               "name" : "message",
+               "description" : "sends a message",
+               "type" : 3,
+               "required" : true
+      }]`
+//This will make our slash command
+})
+
+
+bot.interactionCommand({
+name: "test", //name of the slash command
+prototype : 'slash',
+code: `$interactionReply[Beta Feature Testing Successful!]` // code that will be executed if slash command triggered
+})
+bot.onInteractionCreate()
+
+bot.interactionCommand({ //command
+name: "say", //name of the slash command
+prototype : 'slash',
+code: `$interactionReply[
+$slashOption[message]
+]` // code that will be executed if slash command triggered
+})
+bot.onInteractionCreate() 
+
+//For Bot utilities
 bot.status({
-    text:"Graveyard Shift- Mori Calliope ft BOOGEY VOXX",
-    type:"STREAMING",
-    url:"https://www.youtube.com/watch?v=TIgcVT4SfGw"
+  text: "end of a life- Mori Calliope",
+  type:"STREAMING",
+  url: "https://www.youtube.com/watch?v=BXB26PzV31k"
 });
 
 bot.onMessage({
-    guildOnly : false
+  guildOnly : false
+});
+
+//voice.onTrackStart();
+
+bot.variables({
+  wallet: "0",
+  bank: "0",
+  money: "0",
+  salary:"0",
+  lootbox:"0",
+  gold:"0",
+  loot:"0",
 });
 
 loader.load(bot.cmd, "./commands/dev/");
 loader.load(bot.cmd, "./commands/fun/");
-loader.load(bot.cmd, "./commands/mod/");
+loader.load(bot.cmd, "./commands/mod");
+loader.load(bot.cmd, "./commands/misc");
+loader.load(bot.cmd, "./commands/eco");
+//bot cmds
+//loader.load(voice.cmd, "./commands/voice/"); //voice cmds
 
 keepAlive();
